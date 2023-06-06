d87c1e558c7b4c6728b8d86aa114af6d0dca1fbf
CaritoDaSilvaMeli <carolina.dasilva@mercadolibre.cl>
2023-06-06 14:30:46 -0400

    fix route in service

--- client/src/services/task-service.js
+++ client/src/services/task-service.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-export const getTasks = () => axios.get('http://localhost:8080/api/task');
+export const getTasks = () => axios.get('/api/task');
 
-export const getTask = (id) => axios.get(`http://localhost:8080/api/task/${id}`);
+export const getTask = (id) => axios.get(`/api/task/${id}`);
 
-export const createTask = (task) => axios.post('http://localhost:8080/api/task', task);
+export const createTask = (task) => axios.post('/api/task', task);
 
-export const updateTask = (id, task) => axios.put(`http://localhost:8080/api/task/${id}`, task);
+export const updateTask = (id, task) => axios.put(`/api/task/${id}`, task);
 
-export const deleteTask = (id) => axios.delete(`http://localhost:8080/api/task/${id}`);
+export const deleteTask = (id) => axios.delete(`/api/task/${id}`);
